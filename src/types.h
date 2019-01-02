ed850165336a53a2dffe1c7b07e4a85b97f20413
Karel Břinda <karel.brinda@gmail.com>
2019-01-02 14:09:41 -0500

    Update license

--- src/types.h
+++ src/types.h
@@ -1,3 +1,26 @@
+/* The MIT License
+
+   Copyright (c) 2016-2019 Karel Brinda (kbrinda@hsph.harvard.edu)
+
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to deal
+   in the Software without restriction, including without limitation the rights
+   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+   copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included in all
+   copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+   SOFTWARE.
+*/
+
 #pragma once
 
 #include <unistd.h>
@@ -88,7 +111,8 @@ static const uint8_t nt256_nt16[] = {
 
 // clang-format on
 
-static const uint8_t nt16_nt256[] = "NACMGRSVTWYHKDBN"; // modified, the first character is usually '='
+static const uint8_t nt16_nt256[] =
+    "NACMGRSVTWYHKDBN";  // modified, the first character is usually '='
 
 static const uint8_t nt4_nt256[] = "ACGTN";
 
@@ -100,4 +124,3 @@ static const uint8_t bitsset_table256[256] = {
 #define B4(n) B2(n), B2(n + 1), B2(n + 1), B2(n + 2)
 #define B6(n) B4(n), B4(n + 1), B4(n + 1), B4(n + 2)
     B6(0), B6(1), B6(1), B6(2)};
-
