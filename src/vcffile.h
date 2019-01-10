fdec6b5803e1c5673e8c3e0f6fc29bd2d785a2e6
Karel Břinda <karel.brinda@gmail.com>
2019-01-10 16:20:58 -0500

    Append _ to members of VcfFile

--- src/vcffile.h
+++ src/vcffile.h
@@ -33,20 +33,20 @@
 namespace ococo {
 
 struct VcfFile {
-    std::string fn;
-    FILE *file;
-    Params params;
+    std::string fn_;
+    FILE *file_;
+    Params params_;
 
     VcfFile(std::string fn, Params params)
-        : fn(fn), file(nullptr), params(params) {
-        if (!fn.empty()) {
+        : fn_(fn), file_(nullptr), params_(params) {
+        if (fn.size()) {
             info("Opening the VCF stream ('%s').\n", fn.c_str());
 
             if (fn == std::string("-")) {
-                file = stdout;
+                file_ = stdout;
             } else {
-                file = fopen(fn.c_str(), "w+");
-                if (file == nullptr) {
+                file_ = fopen(fn.c_str(), "w+");
+                if (file_ == nullptr) {
                     fatal_error("Problem with opening the VCF file '%s'.\n",
                                 fn.c_str());
                 }
@@ -57,8 +57,8 @@ struct VcfFile {
     }
 
     ~VcfFile() {
-        if (file != nullptr && fn != "-") {
-            int error_code = fclose(file);
+        if (file_ != nullptr && fn_ != "-") {
+            int error_code = fclose(file_);
             if (error_code != 0) {
                 error("Output VCF file could not be closed.\n");
             }
@@ -66,20 +66,20 @@ struct VcfFile {
     }
 
     void print_header() const {
-        if (file == nullptr) {
+        if (file_ == nullptr) {
             return;
         }
         std::time_t tt = std::time(nullptr);
         tm *tm         = localtime(&tt);
 
-        fprintf(file,
+        fprintf(file_,
                 "##fileformat=VCFv4.3\n"
                 "##fileDate=%04d%02d%02d\n"
                 "##source=Ococo\n",
                 tm->tm_year + 1900, tm->tm_mon + 1, tm->tm_mday);
 
-        if (!params.command_.empty()) {
-            fprintf(file, "##ococo_command=%s\n", params.command_.c_str());
+        if (!params_.command_.empty()) {
+            fprintf(file_, "##ococo_command=%s\n", params_.command_.c_str());
         }
         /*fprintf(file, "##ococo_stats_datatype_size=%zubits\n", 8 * sizeof(T));
         fprintf(file, "##ococo_C=%dbits\n", C);
@@ -93,40 +93,40 @@ struct VcfFile {
                     seq_name[seqid].c_str(), seq_len[seqid]);
         }*/
 
-        fprintf(file,
+        fprintf(file_,
                 "##INFO=<ID=C,Number=4,Type=Integer,Description="
                 "\"A,C,G,T counters.\">\n");
-        fprintf(file,
+        fprintf(file_,
                 "##INFO=<ID=COV,Number=1,Type=Integer,Description="
                 "\"Coverage\">\n");
-        fprintf(file,
+        fprintf(file_,
                 "##INFO=<ID=AF,Number=A,Type=Float,Description="
                 "\"Allele frequency for the ALT allele.\">\n");
         fprintf(
-            file,
+            file_,
             "##INFO=<ID=EX,Number=1,Type=Integer,Description="
             "\"Values are exact (1=yes, 0=no), i.e., no bitshift made\">\n");
-        fprintf(file, "#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\n");
+        fprintf(file_, "#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\n");
     }
 
     void print_substitution(const std::string &seq_name, int64_t pos,
                             char old_base, char new_base,
                             const PosStats &ps) const {
-        if (file == nullptr) {
+        if (file_ == nullptr) {
             return;
         }
 
         const float alt_freq =
-            1.0 * ps.counters_[nt256_nt4[int16_t{new_base}]] / ps.sum_;
+            1.0 * ps.counters_[nt256_nt4[int{new_base}]] / ps.sum_;
 
-        fprintf(file, "%s\t%" PRId64 "\t.\t%c\t%c\t100\tPASS\t",
+        fprintf(file_, "%s\t%" PRId64 "\t.\t%c\t%c\t100\tPASS\t",
                 seq_name.c_str(),  //
                 pos + 1,           //
                 old_base,          //
                 new_base           //
         );
 
-        fprintf(file,
+        fprintf(file_,
                 "C="
                 "%" PRId32       //
                 ",%" PRId32      //
